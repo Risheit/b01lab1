f5856b2510fc3005edef4620ba243faa9eb20535
Risheit <80658104+Risheit@users.noreply.github.com>
2022-09-29 02:16:00 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,67 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Driver {
     public static void main(String[] args) {
-        Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double[] c1 = { 6, 0, 0, 5 };
-        Polynomial p1 = new Polynomial(c1);
-        double[] c2 = { 0, -2, 0, 0, -9 };
-        Polynomial p2 = new Polynomial(c2);
+        Polynomial pNull = new Polynomial();
+        System.out.println(pNull.evaluate(3));
+        
+        double[] cNull = { 0.0, 0.0, 0.0 };
+        int[] eNull = { 0, 1, 2 };
+        Polynomial pNull2 = new Polynomial(cNull, eNull);
+        Polynomial addNull = pNull.add(pNull2);
+        System.out.println("addNull(1.3) = " + addNull.evaluate(1.3));
+        Polynomial multiplyNull = pNull.multiply(pNull2);
+        System.out.println("multiplyNull(2.3) = " + multiplyNull.evaluate(2.3));
+        if (pNull.hasRoot(1))
+            System.out.println("1 is a root of pNull");
+        else
+            System.out.println("1 is not a root of pNull");
+        
+        double[] c1 = { 6, 5 };
+        int[] e1 = {0, 3};
+        Polynomial p1 = new Polynomial(c1, e1);
+        double[] c2 = {-2, -9 };
+        int[] e2 = { 1, 4 };
+        Polynomial p2 = new Polynomial(c2, e2);
         Polynomial s = p1.add(p2);
         System.out.println("s(0.1) = " + s.evaluate(0.1));
+        Polynomial s2 = p1.multiply(p2);
+        System.out.println("s2(0.1) = " + s2.evaluate(0.1));
         if (s.hasRoot(1))
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
+
+        Polynomial addNullToReg = pNull.add(p1);
+        System.out.println("addNullToReg(1) = " + addNullToReg.evaluate(1));
+        Polynomial multiplyNullToReg = pNull.multiply(p1);
+        System.out.println("multiplyNullToReg(1) = " + multiplyNullToReg.evaluate(1));
+
+        double[] c3 = { 10, 2, 3, 9 };
+        int[] e3 = { 0, 2, 3, 5 };
+        Polynomial p3 = new Polynomial(c3, e3);
+        Polynomial s3 = p1.multiply(p3);
+        System.out.println("s3(0.1) = " + s3.evaluate(0.1));
+
+        try {
+            pNull.saveToFile("pNull.txt");
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        try {
+            s.saveToFile("poly.txt");        
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+        File polyFile = new File("poly.txt");
+        try {
+            Polynomial pFile = new Polynomial(polyFile);
+            System.out.println("pFile(0.1) = " + pFile.evaluate(0.1));
+        } catch (FileNotFoundException e) {
+            System.err.println(e);
+        }
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,48 +1,252 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Polynomial {
     public double[] coefficients;
+    public int[] exponents;
+
+    // Lab Functions
 
     public Polynomial() {
-        coefficients = new double[] { 0.0 };
+        coefficients = null;
+        exponents = null;
     }
 
-    public Polynomial(double[] initArray) {
-        coefficients = initArray.clone();
-    }
+    public Polynomial(double[] coefficients, int[] exponents) {
+        this.coefficients = coefficients == null ? null : coefficients.clone();
+        this.exponents = exponents == null ? null : exponents.clone();
 
-    public double[] getCoefficients() {
-        return coefficients;
+        for (var c : this.coefficients) {
+            if (c != 0)
+                return;
+        }
+        
+        this.coefficients = null;
+        this.exponents = null;
     }
+    
+    public Polynomial(File polyFile) throws FileNotFoundException {
+        Pattern noCoefficient = Pattern.compile("^x.*", Pattern.MULTILINE);
+        Pattern noExponent = Pattern.compile(".*x$", Pattern.MULTILINE);
 
-    public int numTerms() {
-        return coefficients.length;
+        var polyString = makeValidPolyString(readFile(polyFile));
+        var polynomial = polyString.split("\\+");
+        
+        ArrayList<Double> coefficients = new ArrayList<>();
+        ArrayList<Integer> exponents = new ArrayList<>();
+        for (var term : polynomial) {
+            if (!term.contains("x")) { // Case N
+                coefficients.add(Double.parseDouble(term));
+                exponents.add(0);
+            } else if (term == "x") { // Case x
+                coefficients.add(1.0);
+                exponents.add(1);
+            } else if (term.matches(noExponent.toString())) { // Case Nx
+                String[] splitTerm = term.split("x");
+                coefficients.add(Double.parseDouble(splitTerm[0]));
+                exponents.add(1);
+            } else if (term.matches(noCoefficient.toString())) { // Case xN
+                String[] splitTerm = term.split("x");
+                coefficients.add(1.0);
+                exponents.add(Integer.parseInt(splitTerm[1]));
+            } else { // Case NxN
+                String[] splitTerm = term.split("x");
+                coefficients.add(Double.parseDouble(splitTerm[0]));
+                exponents.add(Integer.parseInt(splitTerm[1]));
+            }
+        }
+
+        this.coefficients = toDoubleArray(coefficients);
+        this.exponents = toIntArray(exponents);
+        
+        for (var c : this.coefficients) {
+            if (c != 0)
+                return;
+        }
+        
+        this.coefficients = null;
+        this.exponents = null;
     }
 
     public Polynomial add(Polynomial other) {
-        int loopLength = Math.min(this.numTerms(), other.numTerms());
-        double[] polySum = new double[Math.max(this.numTerms(), other.numTerms())];
+        var uniqueExponents = toIntArray(this.getUniqueExponents(other));
 
-        for (int i = 0; i < loopLength; i++) {
-            polySum[i] = this.coefficients[i] + other.coefficients[i];
+        if (uniqueExponents.length == 0)
+            return new Polynomial();
+
+        double[] coefficientSum = new double[uniqueExponents.length];
+        for (int i = 0; i < coefficientSum.length; i++) {
+            coefficientSum[i] = this.getCorrespondingCoefficient(uniqueExponents[i])
+                    + other.getCorrespondingCoefficient(uniqueExponents[i]);
         }
 
-        Polynomial biggest = this.numTerms() > other.numTerms() ? this : other;
-        for (int i = loopLength; i < biggest.numTerms(); i++) {
-            polySum[i] = biggest.coefficients[i];
-        }
-
-        return new Polynomial(polySum);
+        return new Polynomial(coefficientSum, uniqueExponents);
     }
 
     public double evaluate(double x) {
         double sum = 0.0;
         for (int i = 0; i < numTerms(); i++) {
-            sum += coefficients[i] * Math.pow(x, i);
+            sum += coefficients[i] * Math.pow(x, exponents[i]);
         }
 
         return sum;
     }
 
+    public Polynomial multiply(Polynomial other) {
+        double[] newCoefficients = new double[this.numTerms() * other.numTerms()];
+        int[] newExponents = new int[this.numTerms() * other.numTerms()];
+
+        if (newExponents.length == 0)
+            return new Polynomial();
+
+        int k = 0;
+        for (int i = 0; i < this.numTerms(); i++) {
+            for (int j = 0; j < other.numTerms(); j++) {
+                newCoefficients[k] = this.coefficients[i] * other.coefficients[j];
+                newExponents[k] = this.exponents[i] + other.exponents[j];
+                k++;
+            }
+        }
+        
+        return shrinkPolynomial(new Polynomial(newCoefficients, newExponents));
+    }
+
     public boolean hasRoot(double x) {
         return (evaluate(x) == 0);
+    }
+
+    public void saveToFile(String filePath) throws IOException {
+        FileWriter polyFile = new FileWriter(filePath);
+        polyFile.append(this.toString());
+        polyFile.close();
+    }
+
+    // Helper functions 
+
+    public String toString() {
+        StringBuilder polyString = new StringBuilder();
+
+        if (coefficients == null || exponents == null) {
+            return "0";
+        }
+
+        for (int i = 0; i < numTerms(); i++) {
+            if (coefficients[i] != 1) 
+                polyString.append(coefficients[i]);
+                
+            if (exponents[i] != 0) {
+                polyString.append("x");
+                if (exponents[i] != 1)
+                    polyString.append(exponents[i]);
+            }
+
+            if (i + 1 != numTerms() && coefficients[i + 1] >= 0)
+                polyString.append("+");
+        }
+
+        return polyString.toString();
+    }
+
+    public int numTerms() {
+        return exponents == null ? 0 : exponents.length;
+    }
+
+    public double getCorrespondingCoefficient(int exponent) {
+        if (this.exponents == null)
+            return 0.0;
+
+        for (int i = 0; i < numTerms(); i++) {
+            if (this.exponents[i] == exponent)
+                return coefficients[i];
+        }
+
+        return 0.0;
+    }
+
+    private ArrayList<Integer> getUniqueExponents(Polynomial other) {
+        ArrayList<Integer> uniqueExponents = new ArrayList<>();
+
+        for (int exp : this.exponents == null ? new int[]{} : this.exponents) {
+            uniqueExponents.add(exp);
+        }
+        for (int exp : other.exponents == null ? new int[]{} : other.exponents) {
+            if (!uniqueExponents.contains(exp))
+                uniqueExponents.add(exp);
+        }
+
+        uniqueExponents.sort(null);
+        return uniqueExponents;
+    }
+
+    private static int[] toIntArray(ArrayList<Integer> arrayList) {
+        int[] converted = new int[arrayList.size()];
+
+        for (int i = 0; i < converted.length; i++) {
+            converted[i] = arrayList.get(i).intValue();
+        }
+
+        return converted;
+    }
+
+    private static double[] toDoubleArray(ArrayList<Double> arrayList) {
+        double[] converted = new double[arrayList.size()];
+
+        for (int i = 0; i < converted.length; i++) {
+            converted[i] = arrayList.get(i).doubleValue();
+        }
+
+        return converted;
+    }
+
+    private static Polynomial shrinkPolynomial(Polynomial polynomial) {
+        if (polynomial.numTerms() == 0)
+            return polynomial;
+
+        ArrayList<Integer> uniqueExponents = new ArrayList<>();
+        ArrayList<Double> uniqueCoefficients = new ArrayList<>();
+
+        for (int i = 0; i < polynomial.numTerms(); i++) {
+            if (uniqueExponents.contains(polynomial.exponents[i])) {
+                int index = uniqueExponents.indexOf(polynomial.exponents[i]);
+                uniqueCoefficients.set(index, uniqueCoefficients.get(index) + polynomial.coefficients[i]);
+            } else {
+                uniqueExponents.add(polynomial.exponents[i]);
+                uniqueCoefficients.add(polynomial.coefficients[i]);
+            }
+        }
+
+        return new Polynomial(toDoubleArray(uniqueCoefficients), toIntArray(uniqueExponents));
+    }
+
+    private static String readFile(File polyFile) throws FileNotFoundException {
+        Scanner reader = new Scanner(polyFile);
+        String polyString = reader.nextLine();
+        reader.close();
+        
+        return polyString;
+    }
+
+    private static String makeValidPolyString(String polyString) {
+        StringBuilder valid = new StringBuilder(polyString);
+
+        boolean fullyChecked = false;
+        while (!fullyChecked) {
+            for (int i = 0; i < valid.length(); i++) {
+                if (valid.charAt(i) == '-' && valid.charAt(i - 1) != '+') {
+                    valid.insert(i, '+');
+                    fullyChecked = false;
+                    break;
+                }
+
+                fullyChecked = true;
+            }
+        }
+
+        return valid.toString();
     }
 }
